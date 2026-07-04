2877d74ab37eceeef5d7b928aa9d9adf83d9a981
Vaishnavi Gupta <gvaishnavi208@gmail.com>
2026-07-04 15:50:22 +0530

    feat(video): add publish video request validation

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -140,7 +140,26 @@ const getAllVideos = asyncHandler(async(req,res)=>{
     
 })
 
+const publishAVideo = asyncHandler(async (req, res) => {
+    const { title, description} = req.body
+    if(!title?.trim() || !description?.trim() )
+    {
+        throw new ApiError(400,"Title and Description are required")
+    }
+    const videoLocalPath=req.files?.videoFile?.[0]?.path 
+    const thumbnailLocalPath=req.files?.thumbnail?.[0]?.path
+    if(!videoLocalPath){
+        throw new ApiError(400,"Video file is required")
+    }
+    if(!thumbnailLocalPath){
+        throw new ApiError(400,"Thumbnail is required")
+    }
+    
+    
+})
+
 
 export {
-    getAllVideos
+    getAllVideos,
+    publishAVideo
 }
