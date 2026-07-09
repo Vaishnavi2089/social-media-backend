567afe5dd9b60ccec5a7010cbc122696fbd72176
Vaishnavi Gupta <gvaishnavi208@gmail.com>
2026-07-09 21:26:51 +0530

    feat(likes): add toggle video like endpoint

--- src/app.js
+++ src/app.js
@@ -19,7 +19,8 @@ import commentRouter from './routes/comment.routes.js'
 import videoRouter from './routes/video.routes.js'
 import playlistRouter from './routes/playlist.routes.js'
 import dashboardRouter from './routes/dashboard.routes.js'
-import healthcheckRouter from "./routes/healthcheck.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import likeRouter from "./routes/like.routes.js"
 
 //routes declaration
 app.use("/api/v1/users",userRouter)
@@ -27,7 +28,8 @@ app.use("/api/v1/comments",commentRouter)
 app.use("/api/v1/videos",videoRouter)
 app.use("/api/v1/playlist",playlistRouter)
 app.use("/api/v1/dashboard",dashboardRouter)
-app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/likes",likeRouter)
 
 
 
