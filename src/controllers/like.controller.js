84a420d3a2c9244d3d985b00550bb8748ea6ac45
Vaishnavi Gupta <gvaishnavi208@gmail.com>
2026-07-09 22:02:14 +0530

    feat(likes): implement toggle tweet like controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -4,6 +4,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import {Like} from "../models/like.model.js"
 import { Video } from "../models/video.model";
+import { Tweet } from "../models/tweet.model.js";
 
 const toggleVideoLike = asyncHandler(async(req,res)=>{
     const {videoId} = req.params
@@ -93,8 +94,53 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
 
 })
 
+
+const toggleTweetLike = asyncHandler(async (req, res) => {
+    const {tweetId} = req.params
+    if(!mongoose.isValidObjectId(tweetId)){
+        throw new ApiError(400,"Invalid tweet Id")
+    }
+    const tweet = await Tweet.findById(tweetId)
+    if(!tweet){
+        throw new ApiError(404,"Tweet not found")
+    }
+    const existingLike = await Like.findOne({
+        tweet: tweetId,
+        likedBy: req.user._id
+    })
+    if(existingLike){
+        await existingLike.deleteOne()
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    {},
+                    "Tweet unliked successfully"
+                )
+            )
+    }
+    await Like.create({
+        tweet: tweetId,
+        likedBy: req.user._id
+    })
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "Tweet liked successfully"
+            )
+
+        )
+
+}
+)
+
 export {
     toggleVideoLike,
-    toggleCommentLike
+    toggleCommentLike,
+    toggleTweetLike
 }
 
