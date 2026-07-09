985950763ada33891782473b817cf4472087ede5
Vaishnavi Gupta <gvaishnavi208@gmail.com>
2026-07-09 22:33:42 +0530

    feat(likes): add get liked videos route

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { toggleVideoLike,toggleCommentLike ,toggleTweetLike} from "../controllers/like.controller.js";
+import { toggleVideoLike,toggleCommentLike ,toggleTweetLike,getLikedVideos} from "../controllers/like.controller.js";
 
 const router = Router()
 router.use(verifyJWT)
@@ -16,4 +16,8 @@ router
 router
     .route("/toggle/t/:tweetId")
     .post(toggleTweetLike)
+
+router
+    .route("/videos")
+    .get(getLikedVideos)
 export default router
