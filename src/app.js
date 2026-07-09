97e5e86f8ecacebae3a024fe6050ded2b43f0210
Vaishnavi Gupta <gvaishnavi208@gmail.com>
2026-07-09 20:52:42 +0530

    feat(healthcheck): add healthcheck route

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ import commentRouter from './routes/comment.routes.js'
 import videoRouter from './routes/video.routes.js'
 import playlistRouter from './routes/playlist.routes.js'
 import dashboardRouter from './routes/dashboard.routes.js'
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 //routes declaration
 app.use("/api/v1/users",userRouter)
@@ -26,6 +27,7 @@ app.use("/api/v1/comments",commentRouter)
 app.use("/api/v1/videos",videoRouter)
 app.use("/api/v1/playlist",playlistRouter)
 app.use("/api/v1/dashboard",dashboardRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 
 
