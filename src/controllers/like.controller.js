204891d3ad50df5d19221b7e600278b2afed7fb4
Vaishnavi Gupta <gvaishnavi208@gmail.com>
2026-07-09 21:49:44 +0530

    feat(likes): implement toggle comment like controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -39,10 +39,10 @@ const toggleVideoLike = asyncHandler(async(req,res)=>{
         likedBy: req.user._id
     })
     return res
-    .status(201)
+    .status(200)
     .json(
         new ApiResponse(
-            201,
+            200,
             {},
             "Video liked successfully"
         )
@@ -50,7 +50,51 @@ const toggleVideoLike = asyncHandler(async(req,res)=>{
 
 })
 
+const toggleCommentLike = asyncHandler(async (req, res) => {
+    const {commentId} = req.params
+    if(!mongoose.isValidObjectId(commentId)){
+        throw new ApiError(400,"Invalid Comment Id"
+        )
+    }
+    const comment = await Comment.findById(commentId)
+    if(!comment){
+        throw new ApiError(404,"Comment not found")
+    }
+    const existingLike = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user._id
+    })
+    if(existingLike){
+        await existingLike.deleteOne()
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    {},
+                    "Comment unliked successfully"
+                )
+            )
+    }
+    await Like.create({
+        comment: commentId,
+        likedBy: req.user._id
+    })
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "Comment liked successfully"
+            )
+        )
+
+})
+
 export {
-    toggleVideoLike
+    toggleVideoLike,
+    toggleCommentLike
 }
 
