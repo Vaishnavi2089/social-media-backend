1b27b912574156f9bc5116343bafc83a2cfca9b2
Vaishnavi Gupta <gvaishnavi208@gmail.com>
2026-07-10 20:56:20 +0530

    feat(subscription): add toggle subscription route

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ import playlistRouter from './routes/playlist.routes.js'
 import dashboardRouter from './routes/dashboard.routes.js'
 import healthcheckRouter from "./routes/healthcheck.routes.js"
 import likeRouter from "./routes/like.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
 
 //routes declaration
 app.use("/api/v1/users",userRouter)
@@ -30,6 +31,7 @@ app.use("/api/v1/playlist",playlistRouter)
 app.use("/api/v1/dashboard",dashboardRouter)
 app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/likes",likeRouter)
+app.use("/api/v1/subscription",subscriptionRouter)
 
 
 
