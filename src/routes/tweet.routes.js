992b8b939ed8708a083a2a09821d60bb83bd6dbc
Vaishnavi Gupta <gvaishnavi208@gmail.com>
2026-07-11 21:00:25 +0530

    feat(tweet): add delete tweet route

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,20 +1,22 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createTweet, getUserTweets, updateTweet } from "../controllers/tweet.controller.js";
+import { createTweet, deleteTweet, getUserTweets, updateTweet } from "../controllers/tweet.controller.js";
 
 const router = Router()
+router.use(verifyJWT)
 
 
 router
     .route("/")
-    .post(verifyJWT,createTweet)
+    .post(createTweet)
 
 router
     .route("/:userId")
-    .get(verifyJWT,getUserTweets)
+    .get(getUserTweets)
 
 router
     .route("/:tweetId")
-    .patch(verifyJWT,updateTweet)
+    .patch(updateTweet)
+    .delete(deleteTweet)
 
 export default router 
