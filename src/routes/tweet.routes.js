5002e27bf76916cb7dcd8dc55ed1fcc90504baf1
Vaishnavi Gupta <gvaishnavi208@gmail.com>
2026-07-11 19:52:34 +0530

    feat(tweet): add get user tweets route

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createTweet } from "../controllers/tweet.controller.js";
+import { createTweet, getUserTweets } from "../controllers/tweet.controller.js";
 
 const router = Router()
 
@@ -8,5 +8,9 @@ const router = Router()
 router
     .route("/")
     .post(verifyJWT,createTweet)
+
+router
+    .route("/:userId")
+    .get(verifyJWT,getUserTweets)
 
 export default router 
